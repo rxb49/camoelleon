4e1508c74afeb8d4baa859372ac142d46ba71d77
Axel <you@example.com>
2024-12-10 12:57:03 +0100

    ajout de la page login

--- camouelleon/camouelleon/obj/Release/net8.0-windows/camouelleon.AssemblyInfo.cs
+++ camouelleon/camouelleon/obj/Release/net8.0-windows/camouelleon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("camouelleon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e88267b81fc40a7b55d5a8d2b468d296338e3ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6db7bf0c055e9e4206ba9e12a13842ff861f5e3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("camouelleon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("camouelleon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
