dbe8696ac969b54ee317f03523308c649b8db084
Axel <you@example.com>
2024-12-18 21:42:19 +0100

    LaBase

--- camouelleon/camouelleon/obj/Debug/net8.0-windows/camouelleon.AssemblyInfo.cs
+++ camouelleon/camouelleon/obj/Debug/net8.0-windows/camouelleon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("camouelleon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+437ac8a247f1e27e2e63ac590cd194bc23b87b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4eb8d2c650772ebdce01d68d10c925990a831db")]
 [assembly: System.Reflection.AssemblyProductAttribute("camouelleon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("camouelleon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
