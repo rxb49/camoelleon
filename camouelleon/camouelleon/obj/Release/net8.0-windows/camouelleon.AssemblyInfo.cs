9122a8fe541e3b98a3b70a763c852b13196a217e
Robin Tijou <robintijou05@gmail.com>
2025-01-10 13:45:23 +0100

    Sauvegarde des modifications locales avant la fusion

--- camouelleon/camouelleon/obj/Release/net8.0-windows/camouelleon.AssemblyInfo.cs
+++ camouelleon/camouelleon/obj/Release/net8.0-windows/camouelleon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("camouelleon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+437ac8a247f1e27e2e63ac590cd194bc23b87b8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f81b124eb2332f4a135e32fd0a66577cd53b7583")]
 [assembly: System.Reflection.AssemblyProductAttribute("camouelleon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("camouelleon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
