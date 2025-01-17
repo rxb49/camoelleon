324d3eeec606a83b5f042805a06f6b481d2bda65
Axel <you@example.com>
2025-01-17 15:41:14 +0100

    pull du main

--- camouelleon/camouelleon/obj/Debug/net8.0-windows/camouelleon.AssemblyInfo.cs
+++ camouelleon/camouelleon/obj/Debug/net8.0-windows/camouelleon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("camouelleon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bc04a6b380943707d300708a29f7905d36c8b23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd4d02c625705f2df08da4ef6c1f2ebb06822d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("camouelleon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("camouelleon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
